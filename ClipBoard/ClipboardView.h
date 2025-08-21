c01652f40ab26b973186c5fae813b1cada613110
3548848289 <140831634+3548848289@users.noreply.github.com>
2025-08-21 23:02:28 +0800

    Clipboard enhancement (#6) (#8)
    剪切板新增图片的复制黏贴功能
    
    Co-authored-by: cyy-2006 <68464102+cyy-2006@users.noreply.github.com>

--- ClipBoard/ClipboardView.h
+++ ClipBoard/ClipboardView.h
@@ -6,18 +6,23 @@
 #include <QListWidgetItem>
 #include <QMenu>
 #include <QMimeData>
-#include <QDragEnterEvent>
-#include <QStringList>
-#include <QFileDialog>
-#include <QFile>
-#include <QTextStream>
-#include <QMessageBox>
+#include <QScrollArea>
+#include <QLabel>
+#include <QDialog>
+#include <QByteArray>
+#include <QPixmap>
 #include "../manager/include/dbService.h"
 #include "../Setting/include/SettingManager.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class ClipboardView; }
 QT_END_NAMESPACE
+
+class ImagePreviewDialog : public QDialog {
+    Q_OBJECT
+public:
+    ImagePreviewDialog(const QPixmap& pixmap, QWidget *parent = nullptr);
+};
 
 class ClipboardView : public QWidget
 {
@@ -31,23 +36,27 @@ private slots:
     void onClipboardChanged();
     void copyItem();
     void deleteItem();
+    void previewImage();
 
     void on_clearButton_clicked();
     void on_saveButton_clicked();
     void on_listWidget_itemDoubleClicked(QListWidgetItem *item);
-
     void on_listWidget_customContextMenuRequested(const QPoint &pos);
 
 private:
     Ui::ClipboardView *ui;
     QClipboard *clipboard;
     QListWidgetItem *currentRightClickedItem = nullptr;
-    QStringList sessionNewItems;
     dbService& dbservice;
     int initialItemCount = 0;
 
+    // 新增：处理图片路径的辅助函数
+    bool isImageFile(const QString& path);
+    QPixmap loadImageFromPath(const QString& path);
+
+    // 原有辅助函数
+    void addTextItem(const QString& text);
+    void addImageItem(const QPixmap& pixmap);
 };
 
 #endif // CLIPBOARDVIEW_H
-
-
